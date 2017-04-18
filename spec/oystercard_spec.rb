533c0a15a197b7acd1b270c71342440034717f6d
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-18 16:18:25 +0100

    top up feature added with relevant test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
+  
+  context "topping up the card" do
+    it "can top up the balance" do
+      expect { subject.top_up 20 }.to change{ subject.balance }.by 20
+    end
+  end
 end
